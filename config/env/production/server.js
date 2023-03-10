e738f149a94fda2c5c21e3fd5eab442734fb5966
Alasdair Blackwell <aliblackwell@protonmail.com>
2023-03-10 09:49:50 +0000

    Only use custom domain to prevent stm CORS issue

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: "0.0.0.0",
-  url: env("RENDER_EXTERNAL_URL"),
+  url: env("CUSTOM_URL") ?? env("RENDER_EXTERNAL_URL"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET"),
