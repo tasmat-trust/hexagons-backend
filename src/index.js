a2af78be5c9492241d1515df720731cd9d4632e2
Alasdair Blackwell <aliblackwell@protonmail.com>
2023-10-17 18:02:25 +0100

    Reports on server

--- src/index.js
+++ src/index.js
@@ -1,4 +1,95 @@
-'use strict';
+"use strict";
+const {
+  getCurrentLevel,
+  getModuleLabel,
+  sortSubjects,
+} = require("./utils/reports");
+
+const getPupilReport = async (pupilId, groupedSubjects) => {
+  const pupil = await strapi.services["api::pupil.pupil"].findOne(pupilId, {
+    populate: "*",
+  });
+  if (!pupil) {
+    throw new Error("Pupil not found");
+  }
+
+  const levels = await strapi.entityService.findMany("api::level.level", {
+    populate: "*",
+    paginate: {
+      limit: -1,
+    },
+  });
+
+  const levelsWithModules = await Promise.all(
+    levels.map(async (level) => {
+      if (!level.module) return level;
+      const moduleWithCapabilities = await strapi.entityService.findOne(
+        "api::module.module",
+        level.module.id,
+        {
+          populate: ["capabilities"],
+        }
+      );
+
+      return {
+        ...level,
+        module: moduleWithCapabilities,
+      };
+    })
+  );
+
+  const pupilSubjectReports = groupedSubjects
+    .map((subject, i) => {
+      const subjectReportBlock = subject.subjects.map((subject) => {
+        const filteredLevelsBySubject = levelsWithModules.filter((level) => {
+          if (!level.subject) {
+            return false;
+          }
+          return level.subject.id === subject.id;
+        });
+
+        const filteredLevelsByPupil = filteredLevelsBySubject.filter(
+          (level) => {
+            if (!level.pupil) {
+              return false;
+            }
+            return level.pupil.id === pupil.id;
+          }
+        );
+        const withModuleOrders = filteredLevelsByPupil.filter((level) => {
+          return level.module && level.module.order && level.module.level;
+        });
+
+        const withLevelStatus = withModuleOrders.filter((level) => {
+          return level.status;
+        });
+
+        const currentLevel = getCurrentLevel(withLevelStatus);
+        if (currentLevel) {
+          const score = getModuleLabel(currentLevel);
+          return {
+            id: subject.id,
+            subject: subject,
+            score,
+          };
+        }
+        return {
+          id: subject.id,
+          subject: subject,
+          score: 0,
+        };
+      });
+      return subjectReportBlock;
+    })
+    .flat();
+
+  return {
+    id: pupilId,
+    name: pupil?.name ?? "Alan",
+    groupedSubjects,
+    subjectReports: pupilSubjectReports,
+  };
+};
 
 module.exports = {
   /**
@@ -7,7 +98,96 @@ module.exports = {
    *
    * This gives you an opportunity to extend code.
    */
-  register(/*{ strapi }*/) {},
+  register({ strapi }) {
+    const extensionService = strapi.service("plugin::graphql.extension");
+    // Custom query resolver to get all authors and their details.
+    extensionService.use(({ strapi }) => ({
+      typeDefs: `
+            type Query {
+              pupilReport(pupilId: ID!): PupilReport
+              groupReport(groupId: ID!, orgId: ID!): GroupReport
+            }
+    
+            type PupilSubjectReport {
+              id: ID
+              subject: Subject
+              score: Float
+            }
+
+            type ParentSubject {
+              name: String
+              isCore: Boolean
+              subjects: [Subject]
+            }
+    
+            type PupilReport {
+              id: ID
+              name: String              
+              subjectReports: [PupilSubjectReport]
+            }
+
+            type GroupReport {
+              id: ID
+              name: String 
+              groupedSubjects: [ParentSubject]
+              pupils: [PupilReport]
+            }
+    
+          `,
+
+      resolvers: {
+        Query: {
+          pupilReport: {
+            resolve: async (parent, { pupilId }, context) => {
+              const subjects = await strapi.services[
+                "api::subject.subject"
+              ].find();
+              if (!subjects) {
+                throw new Error("Subjects not found");
+              }
+              const groupedSubjects = sortSubjects(subjects.results);
+              return await getPupilReport(pupilId, groupedSubjects);
+            },
+          },
+          groupReport: {
+            resolve: async (parent, { groupId, orgId }, context) => {
+              const subjects = await strapi.services[
+                "api::subject.subject"
+              ].find();
+              if (!subjects) {
+                throw new Error("Subjects not found");
+              }
+              const groupedSubjects = sortSubjects(subjects.results);
+              const group = await strapi.services["api::group.group"].findOne(
+                groupId,
+                {
+                  populate: ["pupils"],
+                }
+              );
+              if (!group) {
+                throw new Error("Group not found");
+              }
+
+              if (group.orgId && group.orgId !== orgId) {
+                throw new Error("Incorrect organisation for this group");
+              }
+
+              const pupilReports = group.pupils.map(async (pupil) => {
+                return await getPupilReport(pupil.id, groupedSubjects);
+              });
+
+              return {
+                id: groupId,
+                name: group.name,
+                groupedSubjects: groupedSubjects,
+                pupils: pupilReports,
+              };
+            },
+          },
+        },
+      },
+    }));
+  },
 
   /**
    * An asynchronous bootstrap function that runs before
---
+++ src/utils/reports.js
@@ -0,0 +1,147 @@
+// Subjects
+
+const byCoreSubjects = (a, b) => {
+  const aNum = a.isCore ? "a" : "b";
+  const bNum = b.isCore ? "a" : "b";
+  return aNum.localeCompare(bNum);
+};
+
+const sortSubjects = (subjects) => {
+  const normalSubjects = subjects
+    .map((s) => (!s.isChildOf ? s : null))
+    .filter((v) => v !== null);
+  // Get all parent subjects (may change in Strapi)
+  const parentSubjectsAll = subjects
+    .map((subject) => subject.isChildOf)
+    .filter((v) => v !== null);
+  const parentSubjects = [...new Set(parentSubjectsAll)];
+  const subjectsWithParents = parentSubjects.map((parent) => {
+    const childSubjects = subjects
+      .map((s) => (s.isChildOf === parent ? s : null))
+      .filter((v) => v !== null);
+    // Now sort childSubjects ensuring any isCore comes first
+    childSubjects.sort(byCoreSubjects);
+    return {
+      name: parent,
+      isCore: childSubjects[0].isCore,
+      subjects: childSubjects,
+    };
+  });
+
+  const rainbowAwards = {
+    name: "Rainbow Awards",
+    isCore: false,
+    subjects: normalSubjects.filter((subject) => subject.isRainbowAward),
+  };
+  const remainingSubjects = {
+    name: "Remaining subjects",
+    isCore: false,
+    subjects: normalSubjects.filter((subject) => !subject.isRainbowAward),
+  };
+
+  subjectsWithParents.sort((a, b) => a.name.localeCompare(b.name));
+
+  subjects = [...subjectsWithParents, remainingSubjects, rainbowAwards];
+
+  subjects.sort(byCoreSubjects);
+  return subjects;
+};
+
+function getPercentComplete(competencies, capabilities) {
+  if (!competencies || !capabilities) return 0;
+  const completeCompetencies = competencies.filter(
+    (comp, i) => comp.status === "complete"
+  );
+  return parseInt((completeCompetencies.length / capabilities.length) * 100);
+}
+
+function calculateCompetenciesForThisLevel(allComps, capabilitiesToMatch) {
+  if (allComps && allComps.length > 0 && capabilitiesToMatch) {
+    const capString = JSON.stringify(capabilitiesToMatch);
+    const competencies = allComps.filter((comp, i) =>
+      capString.includes(comp.capability_fk)
+    );
+    return competencies;
+  }
+  return null;
+}
+
+function getPercentFromStatus(status) {
+  return status === "emerging"
+    ? 25
+    : status === "developing"
+    ? 60
+    : status === "secure"
+    ? 75
+    : 100;
+}
+
+function sortLevelBy(items, level) {
+  return items.sort((a, b) => {
+    const aLevel = a.module.level === level ? "a" : "b";
+    const bLevel = b.module.level === level ? "a" : "b";
+    return aLevel.localeCompare(bLevel);
+  });
+}
+
+function sortLevels(levels) {
+  let sortedLevels = levels;
+  sortedLevels = sortedLevels.sort((a, b) => a.module.order - b.module.order);
+  sortedLevels = sortLevelBy(sortedLevels, "phase");
+  sortedLevels = sortLevelBy(sortedLevels, "band");
+  sortedLevels = sortLevelBy(sortedLevels, "stage");
+  sortedLevels = sortLevelBy(sortedLevels, "step");
+  return sortedLevels;
+}
+
+function getCurrentLevel(jumbledLevels) {
+  const levels = sortLevels(jumbledLevels);
+  // Get highest level with activity
+  const activeLevels = levels.filter((level) => level.status);
+  const level = activeLevels[activeLevels.length - 1]; // get last item
+
+  if (
+    level &&
+    level.wasQuickAssessed === true &&
+    level.status &&
+    level.module
+  ) {
+    level["percentComplete"] = getPercentFromStatus(level.status);
+  }
+
+  if (level && level.status && !level.wasQuickAssessed && level.module) {
+    const competencies = calculateCompetenciesForThisLevel(
+      level.competencies,
+      level.module.capabilities
+    );
+    level["percentComplete"] = getPercentComplete(
+      competencies,
+      level.module.capabilities
+    );
+  }
+
+  return level;
+}
+
+function getNormalisedModuleNumber(level) {
+  // turn from 1-6,1-6 to 1-12
+  return level.module.level === "stage"
+    ? level.module.order + 6
+    : level.module.order;
+}
+
+function getModuleLabel(level) {
+  // Below returns e.g. 1.45, 7.45
+  let normalisedModuleNumber = getNormalisedModuleNumber(level);
+  let label = `${normalisedModuleNumber}.${level.percentComplete}`;
+  if (parseInt(level.percentComplete) === 100) {
+    label = normalisedModuleNumber + 1; // round up to next level if at 100% complete
+  }
+  return label;
+}
+
+module.exports = {
+  getCurrentLevel,
+  getModuleLabel,
+  sortSubjects,
+};
